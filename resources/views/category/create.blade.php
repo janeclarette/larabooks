fd6371378580b70dd9b5c41dd94738d2c845677c
janeclarette <clarette827@gmail.com>
2024-02-18 15:25:51 +0800

    create

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h1>Create Category</h1>
     <form method="post" action="{{ route('category.store') }}">
         @csrf
         @method('post')
         <div>
-            <label>Category Name</label>
-            <input type="text" name="category_name" placeholder="name"/>
+            <label for="category_name">Category Name</label>
+            <input type="text" id="category_name" name="category_name" placeholder="Name"/>
         </div>
-
         <div>
-            <input type="submit" value="create category"/>
+            <input type="submit" value="Create Category"/>
         </div>
-        
     </form>
-</body>
-</html>
+@endsection
